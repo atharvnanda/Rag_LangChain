c2b9788a814685f25c6a81100804b50e13e0e792
Atharv Nanda <atharvnanda2610@gmail.com>
2026-02-03 12:17:08 +0530

    feat: step3 generate embeddings using Ollama

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_ollama import OllamaEmbeddings
 
 #loading data
 loader = DirectoryLoader('./data', glob='**/*.txt', loader_cls=TextLoader)
@@ -22,3 +23,14 @@ chunks = text_splitter.split_documents(documents)
 
 for i,doc in enumerate(chunks[:5]):
     print(f'Chunk {i+1} content:\n{doc.page_content}\n metadata: {doc.metadata}\n')
+
+#embedding
+
+embedding_model = OllamaEmbeddings(
+    model = "nomic-embed-text"
+)
+
+embeddings = embedding_model.embed_documents([chunk.page_content for chunk in chunks])
+
+print(f'Generated {len(embeddings)} embeddings.')
+print(embeddings[0][:5]) 
