9c61971ba2796072d25f6ca8847804b506a76a2c
Atharv Nanda <atharvnanda2610@gmail.com>
2026-02-03 11:50:24 +0530

    added chunking process

---
+++ script.py
@@ -0,0 +1,24 @@
+from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+#loading data
+loader = DirectoryLoader('./data', glob='**/*.txt', loader_cls=TextLoader)
+
+documents = loader.load()
+
+print(f'Loaded {len(documents)} documents.')
+
+for i, doc in enumerate(documents):
+    print(f'Document {i+1} content:\n{doc.page_content}\n metadata: {doc.metadata}\n')
+
+#chunking
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 200,
+    chunk_overlap = 50
+)
+
+chunks = text_splitter.split_documents(documents)
+
+for i,doc in enumerate(chunks[:5]):
+    print(f'Chunk {i+1} content:\n{doc.page_content}\n metadata: {doc.metadata}\n')
